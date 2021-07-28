180e6f6fd68a9a66b85b9f85d02054fba0feefac
Tiago Figueira <tiago.g.figueira@gmail.com>
2021-07-28 00:13:25 -0300

    Introdução ao JavaScript

---
+++ modulo-tres/js/main.js
@@ -0,0 +1,41 @@
+var nome = "Tiago Figueira";
+var idade = 27;
+var idade2 = 10;
+
+//alert(nome + " tem " + idade + " anos!!!");
+
+alert(idade + idade2);
+
+/*
+Usando os números como texto
+
+var idade = "27";
+var idade2 = "10";
+alert(idade + idade2);
+*/
+
+/*
+
+Usando o console do navegador para mostrar as variáveis
+
+var idade = 27;
+var idade2 = 10;
+console.log(nome)
+console.log(idade + idade2);
+*/
+
+/*
+ * Usando replace para strings "alteração de texto"
+ * var = frase = "O Japão é o melhor time do mundo";
+ * console.log(frase) mostrando a frase original
+ * console.log(frase.replace("Japão, "Brasil")); Alterando Japão por Brasil
+ * alert(frase.replace("Japão, "Brasil"));
+ */
+
+/* 
+    Ajustando texto todo para maiúsculo
+    console.log(frase.toUppercase());
+
+    Ajustando texto todo para minúsuclo
+    console.log(frase.toLowerCase());
+*/
